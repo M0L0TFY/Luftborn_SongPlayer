7470836acab9bc7956eb642f839b1eba482c84d9
Mohamed Lotfy <moh.lotfy05@gmail.com>
2026-07-06 23:26:38 +0300

    test(songs): implement unit test for song service

---
+++ Luftborn_Backend/src/test/java/com/luftborn/luftborn_backend/LuftbornBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luftborn.luftborn_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuftbornBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
